e89ec83d82d69aa490b54bd152e6726365f863b8
Subham Kumar <subhamk1978@gmail.com>
2024-10-19 17:35:12 +0530

    changes

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Loading() {
     return (
-        <div className="flex flex-col items-center justify-center fixed min-h-screen bg-gradient-to-r from-[#5c68d3] to-primary w-screen h-screen">
+        <div className="z-40 flex flex-col items-center justify-center fixed min-h-screen bg-gradient-to-r from-[#5c68d3] to-primary w-screen h-screen">
             <div className="relative">
                 <div className="w-24 h-24 border-t-4 border-b-4 border-white rounded-full animate-spin"></div>
                 <div className="w-24 h-24 border-t-4 border-b-4 border-purple-300 rounded-full animate-ping absolute top-0 left-0"></div>
